ea8bc155ffada7931d8023a98576f6f412ac8448
Steve Redisch <sredisch@gannett.com>
2015-01-20 09:04:32 -0500

    removed an unused function from env.rb file

--- features/support/env.rb
+++ features/support/env.rb
@@ -7,10 +7,3 @@ require 'timeout'
 # good warm fuzzy feeling
 
 Capybara.default_driver = :selenium
-
-
-Capybara.register_driver :selenium do |app|
-  http_client = Selenium::WebDriver::Remote::Http::Default.new
-  http_client.timeout = 120
-  Capybara::Selenium::Driver.new(app, :browser => :firefox, :http_client => http_client)
-end
