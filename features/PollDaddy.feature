a17b482acd56e132d66bd78b64f1145c268bc6b4
Steve Redisch <sredisch@gannett.com>
2015-01-21 12:21:17 -0500

    changes made per Faisal

--- features/PollDaddy.feature
+++ features/PollDaddy.feature
@@ -4,13 +4,13 @@ Feature: I would like to use the PollDaddy feature on a USCP site
 @poll
 Scenario: I attempt to find the PollDaddy module
 Given I am on the opinion page of the stage version of a USCP site
-Then I can see the word 'POLL' in the left suspender
+Then I can see the word POLL in the left suspender
   And I can see the PollDaddy module
 
 @viewresults
 Scenario: I can view the results of the poll without voting
 Given I am on the opinion page of the stage version of a USCP site
-When I click on the 'View Results' link
+When I click on the View Results link
 Then I can see the results of the poll
 
 @vote
@@ -22,21 +22,21 @@ Then I can see the results of the poll
 @seeallpolls @recentpollspage
 Scenario: I should be able to see all recent PollDaddy polls
 Given I am on the opinion page of the stage version of a USCP site
-When I click on the 'See All Polls' link
-Then I am taken to the 'Polls' page
+When I click on the See All Polls link
+Then I am taken to the "Polls" page
 
 @yoursay @viewresults
 Scenario: I should be able view results of a recent poll without voting
-Given I am on the 'Your Say' page for a poll
-When I click on the 'View Results' link
+Given I am on the Your Say page for a poll
+When I click on the View Results link
 Then I can see the results of the poll
 
 @polls @recentpollspage
 Scenario: I should be able to vote in the first poll
-Given I am on the 'Polls' page
+Given I am on the Polls page
 When I click on the title of the first poll
-Then I am taken to the 'Your Say' page for that poll
+Then I am taken to the Your Say page for that poll
 When I click on the first option
 Then I can see the poll results for that poll
-When I click on the 'See All Polls' link
-Then I am taken to the 'Polls' page
+When I click on the See All Polls link
+Then I am taken to the "Polls" page
--- features/step_definitions/step_1_definitions.rb
+++ features/step_definitions/step_1_definitions.rb
@@ -1,53 +1,48 @@
 Given (/^I am on the opinion page of the stage version of a USCP site$/) do
   visit('http://ux-preprod-app.democratandchronicle.com/opinion/')
-  sleep 3
 end
 
 And(/^I can see the PollDaddy module$/) do
-  find("[class='polldaddy-poll-suspender card-suspender']")
+  expect(page).to have_selector('.polldaddy-poll-suspender')
 end
 
-Then(/^I can see the word 'POLL' in the left suspender$/) do
+Then(/^I can see the word POLL in the left suspender$/) do
   expect(page).to have_content 'Poll'
 end
 
 When(/^I vote for the first poll choice$/) do
   first('.pmp-answer-option ').click
-  sleep 3
 end
 
 Then(/^I can see the results of the poll$/) do
- find("[class='poll-answer-wrapper pmp-active']")
+  expect(page).to have_selector('.poll-answer-wrapper')
 end
 
-When(/^I click on the 'View Results' link$/) do
+When(/^I click on the View Results link$/) do
   click_link("View Results")
-  sleep 3
 end
 
-When(/^I click on the 'See All Polls' link$/) do
+When(/^I click on the See All Polls link$/) do
   click_link("See All Polls")
-  sleep 3
+  sleep 4
 end
 
-Then(/^I am taken to the 'Polls' page$/) do
-  expect(page).to have_content 'Polls'
+Then(/^I am taken to the "(.*?)" page$/) do |textOnPage|
+  expect(page).to have_content textOnPage
 end
 
-Given(/^I am on the 'Polls' page$/) do
+Given(/^I am on the Polls page$/) do
   visit('http://ux-preprod-app.democratandchronicle.com/polls/all/')
-  sleep 3
   expect(page).to have_content 'Polls'
 end
 
 When(/^I click on the title of the first poll$/) do
   first('.pmp-overlay-poll-link').click
-  sleep 3
 end
 
-Then(/^I am taken to the 'Your Say' page for that poll$/) do
-  find("[class='polls-primary-wrapper']")
-  find("[class='poll-question-wrapper pmp-active']")
+Then(/^I am taken to the Your Say page for that poll$/) do
+  expect(page).to have_selector('.polls-primary-wrapper')
+  expect(page).to have_selector('.poll-question-wrapper')
 end
 
 When(/^I click on the first option$/) do
@@ -55,14 +50,12 @@ When(/^I click on the first option$/) do
 end
 
 Then(/^I can see the poll results for that poll$/) do
-  find("[class='poll-answer-wrapper pmp-active']")
+  expect(page).to have_selector('.poll-answer-wrapper')
 end
 
-Given(/^I am on the 'Your Say' page for a poll$/) do
+Given(/^I am on the Your Say page for a poll$/) do
   visit('http://ux-preprod-app.democratandchronicle.com/polls/all/')
-  sleep 3
   first('.pmp-overlay-poll-link').click
-  sleep 3
-  find("[class='polls-primary-wrapper']")
-  find("[class='poll-question-wrapper pmp-active']")
+  expect(page).to have_selector('.polls-primary-wrapper')
+  expect(page).to have_selector('.poll-question-wrapper')
 end
